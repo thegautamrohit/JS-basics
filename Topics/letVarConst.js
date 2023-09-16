69aac199e91101e326be162a685e3443579f7a51
thegautamrohit <rgautam1997@gmail.com>
2023-09-16 15:44:59 +0530

    Initial commit

---
+++ Topics/letVarConst.js
@@ -0,0 +1,15 @@
+
+
+// console.log(a);
+console.log(b);
+// console.log(c);
+
+let a = 10;
+var b = 20;
+const c = 30;
+
+
+
+
+//a and c gets defined in the seperate memory space during the initialiszation
+//whereas var gets defined in the global space so thats why it is available befor initialisation as undefined 
