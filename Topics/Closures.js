7d68d05aedfb720be87b437a233e9ae4b08fa302
thegautamrohit <rgautam1997@gmail.com>
2023-09-16 17:45:05 +0530

    closure number print

--- Topics/Closures.js
+++ Topics/Closures.js
@@ -6,17 +6,44 @@ function closure() {
   };
 }
 
-let close = closure();
+// let close = closure();
 
-console.log(close());
-console.log(close());
-console.log(close());
-console.log(close());
-console.log(close());
-console.log(close());
-console.log(close());
+// console.log(close());
+// console.log(close());
+// console.log(close());
+// console.log(close());
+// console.log(close());
+// console.log(close());
+// console.log(close());
+
+// let closeNew = closure();
+
+// console.log(closeNew());
+
+function printNum() {
+  for (let i = 1; i <= 5; i++) {
+    setTimeout(() => {
+      console.log(i);
+    }, i * 1000);
+  }
+}
+
+// printNum();
 
 
-let closeNew = closure()
 
-console.log(closeNew());
+
+//USING "var"_______________________________________________//
+
+function printNew() {
+  for (var i = 0; i <= 5; i++) {
+    function close(i) {
+      setTimeout(() => {
+        console.log(i);
+      }, i * 1000);
+    }
+    close(i);
+  }
+}
+
+printNew();
