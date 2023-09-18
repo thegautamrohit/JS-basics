3f4c8f72d0ae09a3af7199283afac3d609789803
thegautamrohit <rgautam1997@gmail.com>
2023-09-18 17:26:43 +0530

    update

--- Topics/Debouncing.js
+++ Topics/Debouncing.js
@@ -1,16 +1,26 @@
-function debounce(fn, t) {
-  let timer;
-  return function () {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      fn();
-    }, t);
-  };
-}
+// function debounce(fn, t) {
+//   let timer;
+//   return function () {
+//     clearTimeout(timer);
+//     timer = setTimeout(() => {
+//       fn();
+//     }, t);
+//   };
+// }
 
 let c = 0;
 const print = () => {
   console.log("Clicked" + " " + ++c);
+};
+
+const debounce = (fn, delay) => {
+  let timer;
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn(...args);
+    }, delay);
+  };
 };
 
 const optimisedFunction = debounce(print, 1000);
